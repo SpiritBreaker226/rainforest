fe641aa1fa4fa68898f922f8b4cfd7a31dee0bb2
Jason Stathopulos <jstathopulos@gmail.com>
2015-02-25 15:06:15 -0500

    Comment Out AJAX & Add jQuery GET AJAX Function
    Comment out jQuery AJAX version and then add in the jQuery GET AJAX
    function which will do the same as AJAX version but uses the Get method
    action type without declaring it.

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -3,12 +3,22 @@ $(document).on("ready page:load", function() {
 		event.preventDefault();
 		var searchValue = $("#search").val();
 
+		/* 
+		jQuery AJAX Version
+
 		$.ajax({
 			url: "/products?search=" + searchValue,
 			type: "GET",
 			dataType: "html"
 		}).done(function(data) {
 			$("#products").html(data);
-		});
+		});*/
+
+		// inside the submit event callback, replace the $.ajax portion with the following
+    $.get("/products?search=" + searchValue)
+    	.done(function(data) {
+    		console.log(data);
+    		$("#products").html(data);
+    	});
 	});
 });
